96e18b8305af49f476dbfb21902640924a166ea0
charles agyemang <micnkru@gmail.com>
2017-03-28 15:20:25 +0000

    schema move

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328013052) do
+ActiveRecord::Schema.define(version: 20170328151428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 
   create_table "mesters", force: :cascade do |t|
     t.string   "name"
