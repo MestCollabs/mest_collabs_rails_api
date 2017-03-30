1c0a071b6cfcae31cd58e708e051ca9046d5d22f
charles agyemang <micnkru@gmail.com>
2017-03-30 15:05:27 +0000

    Added a new model and seeded the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,91 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Mester.create([
+        {
+          email:  "paa@gmail.com",
+          name:   "Paa",
+          profilePicture: "https://www.google.com"
+        },
+        {
+          email:  "duncan@gmail.com",
+          name:   "Duncan",
+          profilePicture: "https://www.google.com"
+        },
+        {
+          email: "mum@gmail.com",
+          name:  "Mum",
+          profilePicture: "https://www.google.com"
+        },
+        {
+          email: "tazz@gmail.com",
+          name:  "Tazz",
+          profilePicture: "https://www.google.com"
+        },
+        {
+          email: "mic@gmail.com",
+          name:  "Micheal",
+          profilePicture: "https://www.google.com"
+        }
+
+  ])
+
+App.create([
+      {
+        appname:  "MestCollabsAndroid",
+        email:    "opokuagyemangcharles@gmail.com",
+        password: "12345678"
+      }
+
+  ])
+
+
+Post.create([
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      },
+      {
+        body:     "It's a good day in mest. I am glad I came here",
+        picture:  "https://www.google.com",
+        mester_id: 1
+      }
+  ])
