271eace613d45fd0f4c8217e3f2694e443736bb8
charles agyemang <micnkru@gmail.com>
2017-04-08 17:08:59 +0000

    error in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -498,5 +498,5 @@ def create_n_feedbacks(n)
 end
 
 create_n_users(53, Mester, "name", "email", "profilePicture")
-create_n_feeds(200, Feed, "body", "picture", "mest_power")
-create_n_feedbacks(300)
+#create_n_feeds(200, Feed, "body", "picture", "mest_power")
+#create_n_feedbacks(300)
